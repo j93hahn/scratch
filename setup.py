4fb12503a060768c58c010d395fc8f3ad33c42e7
Joshua Ahn <joshuajahn2001@gmail.com>
2023-07-03 12:53:55 -0500

    setup complete

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+"""
+Guidelines for setup.py:
+https://packaging.python.org/en/latest/guides/distributing-packages-using-setuptools/#setup-args
+"""
+setup(
+    name='scratch',
+    version='0.1.0',
+    description='Personal scratchpad for research, machine learning, and experimentation.',
+    url='https://github.com/j93hahn/scratch',
+    author='Joshua Ahn',
+    author_email='jjahn@uchicago.edu',
+    packages=find_packages(),
+    python_requires='>=3',
+    install_requires=[],
+)
