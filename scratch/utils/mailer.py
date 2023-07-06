0c0d13de132c98fd4709cacf3b2eded34d597dab
Joshua Ahn <joshuajahn2001@gmail.com>
2023-07-06 14:20:10 -0500

    Update

--- scratch/utils/mailer.py
+++ scratch/utils/mailer.py
@@ -1,3 +1,5 @@
+# modeled from Haochen Wang's code
+
 import os
 import logging
 from contextlib import contextmanager
@@ -24,14 +26,8 @@ def send_email(
     s.quit()
 
 
-"""
-CONDA_DEFAULT_ENV                   # multinerf
-SLURMD_NODENAME                     # gpu32
-os.getcwd().split('/')[-1]          # 23_0703_1812_09 - experiment folder name
-"""
-
 @contextmanager
-def exp_fail(to="jjahn@uchicago.edu", subject=None, do_send=True):
+def warn_email(to="jjahn@uchicago.edu", subject=None, do_send=True):
     """note that contextmanager can be used as a decorator as well"""
     if subject is None:
         subject = f"Experiment {os.getcwd().split('/')[-1]} failed"
@@ -45,8 +41,7 @@ def exp_fail(to="jjahn@uchicago.edu", subject=None, do_send=True):
         pass
 
 
-if __name__ == "__main__":
-    # send_email("test", "test", "jjahn@uchicago.edu")
-    with exp_fail():
+def exp_fail():
+    with warn_email():
         error_msg = f"experiment {os.getcwd().split('/')[-1]} failed on {os.environ.get('SLURMD_NODENAME')} with conda env {os.environ.get('CONDA_DEFAULT_ENV')}"
         raise Exception(error_msg)
