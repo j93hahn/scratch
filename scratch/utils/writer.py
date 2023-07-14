ec86aab94c35b5b235bc9676a864bddb4b436aeb
Joshua Ahn <jjahn@archimedes.ttic.edu>
2023-07-14 14:20:50 -0500

    functional json writer with threading

--- scratch/utils/writer.py
+++ scratch/utils/writer.py
@@ -1,28 +1,85 @@
 import imageio.v2 as iio
+import json
 import os
+import threading
+import time
+import os.path as osp
+import sqlite3
 from typing import Any
 from pathlib import Path
+from datetime import datetime, timedelta
 
 
 class MetricWriter():
-    def __init__(self, output_dir: str='./'):
-        self.output_dir = Path(output_dir)
+    def __init__(self, output_path: str='history.json', sync_period: int=5):
+        # assert output_path.endswith('.json'), \
+        #     f"MetricWriter only supports .json files but got {output_path}."
+        self.output_path = osp.abspath(output_path)
+        self.curr_history = []
+        self._init_writer()
+
+        self.sync_period = timedelta(seconds=sync_period)
+        self._init_ticker()
+
+        # self._establish_database()
+
+    def _establish_database(self):
+        self._conn = sqlite3.connect(self.output_path)
+        self._cursor = self._conn.cursor()
+        self._cursor.execute(
+            'CREATE TABLE test_run (data NUMERIC)'
+        )
+
+    def _init_ticker(self):
+        self._join = False
+        self.last_sync = datetime.now()
+        self.now = self.last_sync
+        self.ticker = threading.Thread(target=self.sync, daemon=True)
+        self.ticker.start()
 
     def _init_writer(self):
-        pass
+        self._writer = open(self.output_path, 'a+', encoding='utf-8')
 
     def add_metric(self, metric: Any):
-        pass
+        """Add a metric to the current history."""
+        self.curr_history.append(metric)
+
+    def flush_history(self):
+        self._cursor.execute(
+            'INSERT INTO test_run (data) self.curr_history'
+        )
+        self._conn.commit()
+        self.curr_history = []
+
+    def flush(self):
+        self._writer.write(json.dumps(self.curr_history))
+        self._writer.flush()
+        self.curr_history = []
+
+    def sync(self):
+        """Synchronizes the current metrics history with the disk."""
+        while not self._join:
+            self.now = datetime.now()
+            if (self.now - self.last_sync) >= self.sync_period:
+                print(self.now - self.last_sync)
+                self.flush()
+                # self.flush_history()
+                self.last_sync = self.now
 
     def close(self):
-        pass
+        if self.curr_history != []:
+            self.flush()
+        self._join = True
+        self.ticker.join()
+        self._writer.close()
+        # self._conn.close()
 
 
 class VideoWriter():
     def __init__(self, output_path: str='video.mp4', fps: int=30):
         assert output_path.endswith('.mp4'), \
             f"VideoWriter only supports .mp4 files but got {output_path}."
-        self.output_path = os.path.abspath(output_path)
+        self.output_path = osp.abspath(output_path)
         self.fps = fps
         self._init_writer()
 
@@ -46,3 +103,12 @@ class VideoWriter():
 
     def close(self):
         self._writer.close()
+
+
+if __name__ == '__main__':
+    x = MetricWriter()
+    for i in range(10):
+        time.sleep(1)
+        x.add_metric(i)
+    print("Closing")
+    x.close()
