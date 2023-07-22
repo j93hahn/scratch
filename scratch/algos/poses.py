6ba04e3c092541e01227cfc705b982bdf2c502cb
Joshua Ahn <joshuajahn2001@gmail.com>
2023-07-22 15:59:59 -0500

    pose calculations update

--- scratch/algos/poses.py
+++ scratch/algos/poses.py
@@ -1,9 +1,21 @@
-import torch
 import numpy as np
+import torch
+import json
+from pathlib import Path
 
+try:
+    import open3d as o3d
+except ImportError:
+    print("open3d not found. Some functions may not work.")
 
 class PoseCalculator():
     def __init__(self, K, R, t):
         self.K = K
         self.R = R
         self.t = t
+
+
+if __name__ == '__main__':
+    with open(Path(__file__).parent / 'transforms.json', 'r') as f:
+        x = json.load(f)
+    breakpoint()
