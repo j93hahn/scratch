3d76ed0672b796fe8ed3857727842108d3e3a1f0
Joshua Ahn <jjahn@archimedes.ttic.edu>
2023-07-25 15:35:10 -0500

    seaborn API integration into split/normal plots

--- scratch/illustrate/violin.py
+++ scratch/illustrate/violin.py
@@ -3,13 +3,95 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 from matplotlib.collections import PolyCollection
-from typing import List
+from typing import List, Optional, Any
+from torch import Tensor
 
 
 class ViolinPlot:
     def __init__(self, nrows=1, ncols=1, figsize=(10, 10)):
         self.ax: plt.Axes
         _, self.ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+
+    def plot(
+        self, vals: List[np.ndarray], labels: List[str], xlabel: str,
+        ylabel: str, title: str, save_path: str, showmeans=False,
+        showmedians=False, showextrema=False
+    ):
+        """Plot a violin plot without hue. Generates vertical violin plots only. Deprecated:
+            use plot_v2() instead. Matplotlib's API lacks the versability and customizability
+            of seaborn's API."""
+        assert len(vals) == len(labels), "vals and labels must have the same length"
+        assert save_path.endswith(".png"), "save_path must end with .png"
+
+        parts = self.ax.violinplot(vals, showmeans=showmeans, showmedians=showmedians, showextrema=showextrema)
+        pc: PolyCollection
+        for pc in parts['bodies']:
+            pc.set_facecolor('#D43F3A')     # set color
+            pc.set_edgecolor('black')       # set edge color
+            pc.set_alpha(1)                 # set opacity
+
+        q1, med, q3, w_min, w_max = self.compute_percentiles(vals)
+        inds = np.arange(1, len(med) + 1)
+        self.ax.scatter(inds, med, marker='o', color='white', s=5, zorder=3)
+        self.ax.vlines(inds, q1, q3, color='k', linestyle='-', lw=5)
+        self.ax.vlines(inds, w_min, w_max, color='k', linestyle='-', lw=1)
+
+        self.set_x_axis_style(labels)
+        self.ax.set_title(title)
+        self.ax.set_xlabel(xlabel)
+        self.ax.set_ylabel(ylabel)
+        plt.savefig(save_path, dpi=300)
+        self._clear()
+
+    def plot_v2(
+        self, labels: List[str], data: List[Any], x: str, y: str, title: str,
+        xlabel: str, ylabel: str, path: str, palette: str="Set2"
+    ):
+        """Plot a violin plot without hue. Generates vertical violin plots only. Mirror
+            implementation of plot() above, but using seaborn's API."""
+        df = self._extract_pandas_df(split=False, labels=labels, data=data)
+        assert x in labels and y in labels
+        sns.violinplot(data=df, x=x, y=y, palette=palette, ax=self.ax)
+        self.ax.set_title(title)
+        self.ax.set_xlabel(xlabel)
+        self.ax.set_ylabel(ylabel)
+        plt.savefig(path, dpi=300)
+        self._clear()
+
+    def split_plot(
+        self, labels: List[str], data: List[Any], data2: List[Any],
+        x: str, y: str, hue: str, title: str, xlabel: str, ylabel: str,
+        path: str, palette: str="Set2", legend_loc: str="upper right"
+    ):
+        """Plot a violin plot with hue, a.k.a. side-by-side split violin plots. Generates
+            vertical violin plots only.
+
+        Args:
+            labels (List[str]): the labels for the data
+            data (List[Any]): the data to plot
+            data2 (List[Any]): the second data to plot
+            x (str): the variable where we observe the distributions
+            y (str): the variable whose distribution is to be visualized
+            hue (str): the variable to split the data by
+            title (str): the title of the plot
+            xlabel (str): the x-axis label
+            ylabel (str): the y-axis label
+            path (str): the path to save the plot
+            palette (str, optional): the color palette. Defaults to 'Set2'.
+            legend_loc (str, optional): the location of the legend. Defaults to 'upper right'.
+
+        Returns:
+            None. Produces a split violin plot along the specified orientation.
+        """
+        df = self._extract_pandas_df(split=True, labels=labels, data=data, data2=data2)
+        assert x in labels and y in labels and hue in labels
+        sns.violinplot(data=df, x=x, y=y, hue=hue, split=True, palette=palette, ax=self.ax)
+        self.ax.set_title(title)
+        self.ax.set_xlabel(xlabel)
+        self.ax.set_ylabel(ylabel)
+        self.ax.legend(loc=legend_loc)
+        plt.savefig(path, dpi=300)
+        self._clear()
 
     @staticmethod
     def adjacent_values(vals, q1, q3):
@@ -40,50 +122,24 @@ class ViolinPlot:
             whiskers_min, whiskers_max = whiskers[:, 0], whiskers[:, 1]
             return quartile1, medians, quartile3, whiskers_min, whiskers_max
 
-    def plot(self,
-        vals: List[np.ndarray],
-        labels: List[str],
-        xlabel: str,
-        ylabel: str,
-        title: str,
-        save_path: str,
-        showmeans=False,
-        showmedians=False,
-        showextrema=False
-    ):
-        assert len(vals) == len(labels), "vals and labels must have the same length"
-        assert save_path.endswith(".png"), "save_path must end with .png"
+    def _extract_pandas_df(self, split: bool, labels: List[str], data: List[Any], data2: Optional[List[Any]] = None) -> pd.DataFrame:
+        """Extract a pandas DataFrame from the data and labels."""
+        assert len(labels) == len(data), f"labels and data must have the same length, got {len(labels)} and {len(data)}"
+        df = pd.DataFrame(dict(zip(labels, data)))
+        if split:   # concatenate data2 to df; the hue variable will split the data into two groups
+            assert data2 is not None, "data2 must be provided when split is True"
+            assert len(data) == len(data2), f"data and data2 must have the same length, got {len(data)} and {len(data2)}"
+            _df2 = pd.DataFrame(dict(zip(labels, data2)))
+            df = pd.concat([df, _df2])
 
-        parts = self.ax.violinplot(vals, showmeans=showmeans, showmedians=showmedians, showextrema=showextrema)
-        pc: PolyCollection
-        for pc in parts['bodies']:
-            pc.set_facecolor('#D43F3A')     # set color
-            pc.set_edgecolor('black')       # set edge color
-            pc.set_alpha(1)                 # set opacity
+        for i in range(len(labels)):    # ensure each cell contains one piece of information
+            df = df.explode(labels[i])
+            if isinstance(data[i], list) and (isinstance(data[i][0], float) or \
+                isinstance(data[i][0], np.ndarray) or isinstance(data[i][0], Tensor)):
+                # convert dtype to float from object if list of floats, np.ndarray, or torch.tensor
+                df[labels[i]] = df[labels[i]].astype(float)
 
-        q1, med, q3, w_min, w_max = self.compute_percentiles(vals)
-        inds = np.arange(1, len(med) + 1)
-        self.ax.scatter(inds, med, marker='o', color='white', s=5, zorder=3)
-        self.ax.vlines(inds, q1, q3, color='k', linestyle='-', lw=5)
-        self.ax.vlines(inds, w_min, w_max, color='k', linestyle='-', lw=1)
-
-        self.set_x_axis_style(labels)
-        self.ax.set_title(title)
-        self.ax.set_xlabel(xlabel)
-        self.ax.set_ylabel(ylabel)
-        plt.savefig(save_path, dpi=300)
-        self._clear()
-
-    def hue_plot(self, df: pd.DataFrame, x: str, y: str, hue: str, palette: str = "Set2"):
-        """Plot a violin plot with hue, a.k.a. side-by-side violin plots. Unfinished function."""
-        sns.violinplot(ax=self.ax, data=df, x=x, y=y, hue=hue, palette=palette)
-        self.ax.set_title(f"{y} vs {x} with hue {hue}")
-        self.ax.set_xlabel(x)
-        self.ax.set_ylabel(y)
-        self.ax.legend(loc="upper right")
-        self.ax.grid(True)
-        plt.savefig(f"{y}_vs_{x}_with_hue_{hue}.png", dpi=300)
-        self._clear()
+        return df
 
     def _clear(self):
         """Clear the plot."""
@@ -92,3 +148,12 @@ class ViolinPlot:
     def __del__(self):
         """Close the plot."""
         plt.close()
+
+
+if __name__ == '__main__':
+    x = ViolinPlot()
+    x.split_plot(['layer', 'distribution', 'label'], [[0, 1, 2, 3, 4], [np.random.randn(50) for _ in range(5)], 'coarse'], \
+                [[0, 1, 2, 3, 4], [np.random.randn(50) for _ in range(5)], 'fine'], 'layer', 'distribution', 'label', \
+                'Visualizing Activation Statistics by Layer', 'Layer', 'Activation Distribution', 'split_violin.png')
+    x.plot_v2(['layer', 'distribution'], [[0, 1, 2, 3, 4], [np.random.randn(50) for _ in range(5)]], 'layer', 'distribution', \
+            'Visualizing Activation Statistics by Layer', 'Layer', 'Activation Distribution', 'violin.png')
