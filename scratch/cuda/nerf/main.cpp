900335c4b6e9bca26e936c9a12932b58c5332d1b
Joshua Ahn <jjahn@archimedes.ttic.edu>
2023-09-21 21:12:49 -0500

    cleanup code

--- scratch/cuda/nerf/main.cpp
+++ scratch/cuda/nerf/main.cpp
@@ -7,7 +7,5 @@ int main(int argc, char* argv[]) {
     std::cout << "Begin training!" << std::endl;
     torch::manual_seed(69250713);   // MNJ's random seed
 
-    std::string conf_path = "./runtime_config.yaml";
-    auto exp_runner = std::make_unique<ExpRunner>(conf_path);
     return 0;
 }
