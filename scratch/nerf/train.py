b4467b485edef98b6a46123a2ebc1276ab9f38b1
Joshua Ahn <joshuajahn2001@gmail.com>
2024-02-27 00:08:36 -0600

    train.py done

--- scratch/nerf/train.py
+++ scratch/nerf/train.py
@@ -1,25 +1,17 @@
-import torch
-from internal import fields
 from internal import configs
-from internal import datasets
-from internal import train_utils
+from internal import engine
 from lightning import seed_everything
+from absl import app
 import gin
-import tyro
-from absl import flags, app
-from scratch.utils.wizard import WandbWizard
-import jax
 
 
 def main():
-    config = configs.load_config()
-    seed_everything(config.seed)
-    wizard = WandbWizard(config.name, config.save_dir)
-    dataset = datasets.dataset_dict[config.dataset](config.data_dir)
-    scheduler =
+    configs.define_flags()
+    with gin.config_scope('train'):
+        config = configs.load_config()
+        seed_everything(config.seed)
+        engine.launch(config, train=True)
 
 
 if __name__ == '__main__':
-    configs.define_flags()
-    with gin.config_scope('train'):
-        app.run(main)
+    app.run(main)
