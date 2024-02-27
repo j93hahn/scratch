580f4fa1f130946249a44f47b196d3c3068bd89f
Joshua Ahn <joshuajahn2001@gmail.com>
2024-02-27 00:11:06 -0600

    no need for seeding; evaluation is deterministic in NeRF

--- scratch/nerf/eval.py
+++ scratch/nerf/eval.py
@@ -1,6 +1,5 @@
 from internal import configs
 from internal import engine
-from lightning import seed_everything
 from absl import app
 import gin
 
@@ -9,7 +8,6 @@ def main():
     configs.define_flags()
     with gin.config_scope('eval'):
         config = configs.load_config()
-        seed_everything(config.seed)
         engine.launch(config, train=False)
 
 
