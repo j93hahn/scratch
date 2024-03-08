5a9adc802b24ca365be812cc5e6172afaea028f3
Joshua Ahn <joshuajahn2001@gmail.com>
2024-03-08 12:59:44 -0600

    movie integration into wizard figured out

--- scratch/utils/wizard.py
+++ scratch/utils/wizard.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from typing import Optional
 from pathlib import Path
+from einops import rearrange
 
 
 class WandbWizard():
@@ -47,3 +48,7 @@ if __name__ == "__main__":
     for i in range(0, 100, 5):
         wizard.log(step=i, a=i, b=i**2)
         wizard.save_images(step=i, img=np.random.rand(64, 64, 3))
+        if i == 50:
+            movie = (np.clip(np.random.randn(200, 800, 800, 3), 0, 1) * 255).astype(np.uint8)
+            movie = rearrange(movie, 't h w c -> t c h w')
+            wizard.save_movies(step=i, movie=movie)
