10a7a7760db8eba87b5735eab8bb7487b8e7bb07
Joshua Ahn <joshuajahn2001@gmail.com>
2024-03-09 23:16:48 -0600

    log distributions, make methods staticmethods

--- scratch/utils/wizard.py
+++ scratch/utils/wizard.py
@@ -27,25 +27,37 @@ class WandbWizard():
     def update_config(self, **config_dict) -> None:
         wandb.config.update(config_dict, allow_val_change=True)
 
-    def log(self, step=None, **kwargs):
+    @staticmethod
+    def log_scalars(step=None, **kwargs):
         wandb.log(kwargs, step=step)
 
-    def save_images(self, step=None, **kwargs):
+    @staticmethod
+    def log_distributions(step=None, **kwargs):
+        for name, value in kwargs.items():
+            assert isinstance(value, np.ndarray), "Statistics must be numpy arrays"
+            wandb.log({name: wandb.Histogram(value)}, step=step)
+
+    @staticmethod
+    def log_images(step=None, **kwargs):
         for name, value in kwargs.items():
             wandb.log({name: wandb.Image(value)}, step=step)
 
-    def save_movies(self, step=None, **kwargs):
+    @staticmethod
+    def log_movies(step=None, **kwargs):
         for name, value in kwargs.items():
             wandb.log({name: wandb.Video(value)}, step=step)
+
+    def finish(self):
+        wandb.finish()
 
 
 if __name__ == "__main__":
     config = {"lr": 1e-3, "step_size": 5, "optimizer": "adam"}
     wizard = WandbWizard(project="test", name="test_run", config=config)
     for i in range(0, 100, 5):
-        wizard.log(step=i, a=i, b=i**2)
-        wizard.save_images(step=i, img=np.random.rand(64, 64, 3))
+        wizard.log_scalars(step=i, a=i, b=i**2)
+        wizard.log_images(step=i, img=np.random.rand(64, 64, 3))
         if i == 50:
-            movie = (np.clip(np.random.randn(200, 800, 800, 3), 0, 1) * 255).astype(np.uint8)
+            movie = (np.clip(np.random.randn(40, 60, 60, 3), 0, 1) * 255).astype(np.uint8)
             movie = rearrange(movie, 't h w c -> t c h w')
-            wizard.save_movies(step=i, movie=movie)
+            wizard.log_movies(step=i, movie=movie)
